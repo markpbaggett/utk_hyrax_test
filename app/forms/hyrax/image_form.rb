817e6c7ec8c342c3fa814947ed6fc3d615f38373
Mark Baggett <mbagget1@utk.edu>
2020-07-20 15:37:25 -0400

    Rename data provider and drop year.

--- app/forms/hyrax/image_form.rb
+++ app/forms/hyrax/image_form.rb
@@ -5,9 +5,8 @@ module Hyrax
   class ImageForm < Hyrax::Forms::WorkForm
     self.model_class = ::Image
     self.terms += [:resource_type]
-    self.terms += [:year]
-    self.terms += [:dataprovider]
+    self.terms += [:provider]
     self.required_fields -= [:keyword]
-    self.required_fields += [:dataprovider]
+    self.required_fields += [:provider]
   end
 end
