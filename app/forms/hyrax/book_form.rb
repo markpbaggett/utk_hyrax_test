2f637247dcbcf65574a8b7818fc26ea89d255638
Mark Baggett <mbagget1@utk.edu>
2020-07-21 11:53:43 -0400

    No longer require keyword for books.

--- app/forms/hyrax/book_form.rb
+++ app/forms/hyrax/book_form.rb
@@ -6,6 +6,7 @@ module Hyrax
     self.model_class = ::Book
     self.terms += [:resource_type]
     self.terms += [:provider]
+    self.required_fields -= [:keyword]
     self.required_fields += [:provider]
   end
 end
