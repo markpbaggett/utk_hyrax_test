cb3198fa9f39a3dfe1c45b882222aa946c3c3091
Mark Baggett <mbagget1@utk.edu>
2020-07-21 20:33:43 -0400

    Add unit test and delegation for provider.

--- spec/presenters/hyrax/image_presenter_spec.rb
+++ spec/presenters/hyrax/image_presenter_spec.rb
@@ -1,9 +1,31 @@
-# Generated via
-#  `rails generate hyrax:work Image`
 require 'rails_helper'
 
 RSpec.describe Hyrax::ImagePresenter do
-  it "has tests" do
-    skip "Add your tests here"
+  subject { presenter }
+
+  let(:title) { ['Journey to Skull Island'] }
+  let(:creator) { ['Quest, Jane'] }
+  let(:provider) { "University of Tennessee. Libraries" }
+  let(:visibility) { Hydra::AccessControls::AccessRight::VISIBILITY_TEXT_VALUE_PUBLIC }
+  let :image do
+    Image.new(
+      title: title,
+      creator: creator,
+      provider: provider,
+      visibility: visibility
+    )
+  end
+
+  let(:ability) { Ability.new(user) }
+
+  let(:solr_document) { SolrDocument.new(image.to_solr) }
+
+  let(:presenter) do
+    described_class.new(solr_document, nil)
+  end
+
+  it "delegates provider to solr document" do
+    expect(solr_document).to receive(:provider)
+    presenter.provider
   end
 end
